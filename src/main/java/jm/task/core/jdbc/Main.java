a4519e3607310fd41030f8e07a3b441c981200cb
joozahh <joozah@mail.ru>
2023-11-27 21:49:50 +0300

    some fixes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,16 +3,10 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = Util.getConnection();
         UserService us = new UserServiceImpl();
         us.createUsersTable();
         us.saveUser("Anime", "Naruto", (byte) 23);
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,6 @@
 package jm.task.core.jdbc.util;
-
-
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/somebd";
